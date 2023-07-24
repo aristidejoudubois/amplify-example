15c7772280065c1dbc1c9fd9f935ae04c0aae606
aristidejou <106462048+aristidejou@users.noreply.github.com>
2023-07-24 02:23:35 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Welcome To <span className="text-[hsl(280,100%,70%)]">Aristide Jou</span> Website
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
@@ -25,9 +25,11 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Who is MRC GLOBAL →</h3>
+              <h3 className="text-2xl font-bold">Who is Aristide Jou →</h3>
               <div className="text-lg">
-                MRC GLOBAL is the best place to work.
+                Devops Enginner with a Master degree in Computer science.
+                Linux Administrator, Expert in Github actions, Jenkins,
+                Kubernetes, Terraform, AWS
               </div>
             </Link>
             <Link
@@ -35,14 +37,14 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <h3 className="text-2xl font-bold">My Resume →</h3>
               <div className="text-lg">
-                ALL info about MRC GLOBAL
+                ALL info about Me
               </div>
             </Link>
           </div>
           <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            {hello.data ? hello.data.greeting : "Welcome to MRC GLOBAL..."}
           </p>
         </div>
       </main>
