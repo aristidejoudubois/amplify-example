e80b35a8d15b2f498aea823986538b4bb0f24b5e
aristidejou <106462048+aristidejou@users.noreply.github.com>
2023-07-24 02:36:41 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,12 +27,23 @@ const Home: NextPage = () => {
             >
               <h3 className="text-2xl font-bold">Who is Aristide Jou →</h3>
               <div className="text-lg">
-                Devops Enginner with a Master degree in Computer science.
-                Linux Administrator, Expert in Github actions, Jenkins,
-                Kubernetes, Terraform, AWS
+                Devops Engineer with a Masters degree in Computer science.
+                Background in Linux Administrator, Expert in Github actions, Jenkins,
+                Kubernetes, Terraform, AWS cloud practitioner certificate
               </div>
             </Link>
             <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://create.t3.gg/en/introduction"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Certification →</h3>
+              <div className="text-lg">
+                1- AWS cloud Practitioner Certicate
+              </div>
+            </Link>
+          </div>
+           <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/introduction"
               target="_blank"
