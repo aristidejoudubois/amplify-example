3c85fe460105a0355fdd7218fe068ca058a4a7ee
aristidejou <106462048+aristidejou@users.noreply.github.com>
2023-07-24 01:54:37 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,10 +25,9 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">Who is MRC GLOBAL →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                MRC GLOBAL is the best place to work.
               </div>
             </Link>
             <Link
@@ -38,8 +37,7 @@ const Home: NextPage = () => {
             >
               <h3 className="text-2xl font-bold">Documentation →</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                ALL info about MRC GLOBAL
               </div>
             </Link>
           </div>
